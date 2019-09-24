0cc68eab04ce99151e207b1fbc9d178ce9733dea
Justin.LeCheminant <justin.lecheminant@nordstrom.com>
2019-09-24 10:53:09 -0600

    Added some more utilities work

--- RetroGaming/Utilities.cpp
+++ RetroGaming/Utilities.cpp
@@ -1,9 +1,42 @@
 #include "Utilities.h"
 
+/// <summary>
+/// Gets the random index.
+/// </summary>
+/// <param name="max">The maximum.</param>
+/// <returns></returns>
 int getRandomIndex(long max)
 {
 	random_device random_device;
 	mt19937 engine{ random_device() };
 	uniform_int_distribution<int> dist(0, max);
 	return dist(engine);
+}
+
+/// <summary>
+/// Loads the words.
+/// </summary>
+/// <param name="fileName">Name of the file.</param>
+/// <returns></returns>
+unique_ptr<vector<string>> loadWords(string fileName)
+{
+	vector<string> words;
+
+	fstream file;
+
+	file.open(fileName, ios::out);
+
+	if (file.is_open())
+	{
+		string line = "";
+
+		while (getline(file, line))
+		{
+			words.push_back(line);
+		}
+	}
+
+	file.close();
+
+	return make_unique<vector<string>>(words);
 }
--- RetroGaming/Utilities.h
+++ RetroGaming/Utilities.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <random>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int getRandomIndex(long max);
+
+unique_ptr<vector<string>> loadWords(string fileName);
